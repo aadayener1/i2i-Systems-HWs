a399c844599cae524fdc891d7e1df6a321e5a775
aadayener1 <adayener@gmail.com>
2023-07-20 13:55:07 +0300

    Hazelcast List fix

--- Hazelcast/src/main/java/org/hazelcast_vs_oracledb/hazelcastManager.java
+++ Hazelcast/src/main/java/org/hazelcast_vs_oracledb/hazelcastManager.java
@@ -1,24 +1,27 @@
 package org.hazelcast_vs_oracledb;
 
 import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.collection.IList;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.List;
+import java.util.Random;
 
 public class hazelcastManager {
     //defining number for getter
     private int number;
-    //create list and client for every method
+    //create list and Instance for every method
     private HazelcastInstance hz;
-    private List list;
+    private IList<Object> list;
 
-    private int randomNum;
+    private Random random = new Random();
 
     public long dataAppender(int number){
 
         this.number = number;
-        //defining new client and list for attempt
-        this.hz = HazelcastClient.newHazelcastClient();
+        //defining new Instance and list for attempt
+        this.hz = Hazelcast.newHazelcastInstance();
         this.list = hz.getList("my-distributed-list1");
         //initial time of process
         long initialT = System.currentTimeMillis();
@@ -31,26 +34,26 @@ public class hazelcastManager {
         long lastT = System.currentTimeMillis();
 
 
-        hz.shutdown();
+
         return lastT - initialT;
     }
     public long dataGetter(int number){
-        this.randomNum = ThreadLocalRandom.current().nextInt(1, 1000 + 1);
+
         this.number = number;
-        //defining new client and list for attempt
-        this.hz = HazelcastClient.newHazelcastClient();
+        //defining new Instance and list for attempt
+        this.hz = Hazelcast.newHazelcastInstance();
         this.list = hz.getList("my-distributed-list1");
         //initial time of process
         long initialT = System.currentTimeMillis();
 
         for(int i = 0; i < number; i++){
-            list.get(randomNum);
+            list.get(number-1);
 
         }
         //end time of process
         long lastT = System.currentTimeMillis();
 
-        hz.shutdown();
+
         return lastT - initialT;
 
     }

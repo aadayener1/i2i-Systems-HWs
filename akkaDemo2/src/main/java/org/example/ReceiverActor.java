44c7b99549e5142eb25de9006ef1c1f6d0636997
aadayener1 <adayener@gmail.com>
2023-07-21 14:21:04 +0300

    Modified log message to print

--- akkaDemo2/src/main/java/org/example/ReceiverActor.java
+++ akkaDemo2/src/main/java/org/example/ReceiverActor.java
@@ -1,16 +1,14 @@
 package org.example;
 
 import akka.actor.AbstractActor;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
 
 public class ReceiverActor extends AbstractActor {
-    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(String.class, message -> {
-                    log.info("Received message: {}", message);
+                    System.out.println("Received message: "+ message);
 
                     // Send a message back to the sender actor in akkaDemo1
                     getSender().tell("Hi from Actor2", getSelf());
